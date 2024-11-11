0d017a4d6f98a7401e3a6e9a099113639adb9de4
Brandon and Brenda <phat@biden.co.uk>
2024-11-10 18:49:03 -0600

    main py for dev purposes

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ url = "https://www.bbc.com/news/world-asia-56900013"
 print("Categorizing URL...")
 category = categorize_url(url)
 
+print(category)
+
 print("Retrieving text...")
 text = retrieve_text(url, category)
 
+print(text)
+
 print("Summarizing text...")
 summary = summarize(text, category)
+
+print(summary)
