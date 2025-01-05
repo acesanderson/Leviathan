8770595c0d6ca577e59fa3ec14c46eca9a378b02
Brandon and Brenda <phat@biden.co.uk>
2025-01-04 19:16:31 -0600

    bug squashing, entry points fixed, v3.5

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="Leviathan",
-    version="2.0",
+    version="2.5",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
             "leviathan=Leviathan.leviathan.leviathan:main",
             "sto=Leviathan.save_to_obsidian.save_to_obsidian:main",
             "tutorialize=Leviathan.tutorialize.tutorialize:main",
-            "cookbook=Leviathana.cookbook.cookbook:main",
+            "cookbook=Leviathan.cookbook.cookbook:main",
         ],
     },
 )
