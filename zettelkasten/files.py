5d202afcb9ff3155b4aaef561b18d8046167e271
Brian Anderson <brian.briananderson@gmail.com>
2025-06-09 18:43:26 -0500

    zettelkasten, old code

--- zettelkasten/files.py
+++ zettelkasten/files.py
@@ -1,3 +1,10 @@
 from pathlib import Path
 
 downloads_folder = Path("/Users/bianders/Downloads/")
+
+files = downloads_folder.glob("*.*")
+files = list(files)
+
+extensions = {file.suffix for file in files}
+for e in extensions:
+    print(e)
